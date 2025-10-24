dedc51c2f0a8e333750d9f7476de0196658fe662
Travis Pierce <46234513+XuulMedia@users.noreply.github.com>
2025-10-24 13:29:11 -0600

    Added some new rich text methods for strings

--- Utility Scripts/Extensions/StringExtensions.cs
+++ Utility Scripts/Extensions/StringExtensions.cs
@@ -2,97 +2,142 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ShoelaceStudios.Utilities.Extensions {
-    public static class StringExtensions {
-        /// <summary>Checks if a string is Null or white space</summary>
-        public static bool IsNullOrWhiteSpace(this string val) => string.IsNullOrWhiteSpace(val);
+namespace ShoelaceStudios.Utilities.Extensions
+{
+	public static class StringExtensions
+	{
+		/// <summary>Checks if a string is Null or white space</summary>
+		public static bool IsNullOrWhiteSpace(this string val) => string.IsNullOrWhiteSpace(val);
 
-        /// <summary>Checks if a string is Null or empty</summary>
-        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+		/// <summary>Checks if a string is Null or empty</summary>
+		public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
 
-        /// <summary>Checks if a string contains null, empty or white space.</summary>
-        public static bool IsBlank(this string val) => val.IsNullOrWhiteSpace() || val.IsNullOrEmpty();
+		/// <summary>Checks if a string contains null, empty or white space.</summary>
+		public static bool IsBlank(this string val) => val.IsNullOrWhiteSpace() || val.IsNullOrEmpty();
 
-        /// <summary>Checks if a string is null and returns an empty string if it is.</summary>
-        public static string OrEmpty(this string val) => val ?? string.Empty;
+		/// <summary>Checks if a string is null and returns an empty string if it is.</summary>
+		public static string OrEmpty(this string val) => val ?? string.Empty;
 
-        /// <summary>
-        /// Shortens a string to the specified maximum length. If the string's length
-        /// is less than the maxLength, the original string is returned.
-        /// </summary>
-        public static string Shorten(this string val, int maxLength) {
-            if (val.IsBlank()) return val;
-            return val.Length <= maxLength ? val : val.Substring(0, maxLength);
-        }
+		/// <summary>
+		/// Shortens a string to the specified maximum length. If the string's length
+		/// is less than the maxLength, the original string is returned.
+		/// </summary>
+		public static string Shorten(this string val, int maxLength)
+		{
+			if (val.IsBlank()) return val;
 
-        /// <summary>Slices a string from the start index to the end index.</summary>
-        /// <result>The sliced string.</result>
-        public static string Slice(this string val, int startIndex, int endIndex) {
-            if (val.IsBlank()) {
-                throw new ArgumentNullException(nameof(val), "Value cannot be null or empty.");
-            }
+			return val.Length <= maxLength ? val : val.Substring(0, maxLength);
+		}
 
-            if (startIndex < 0 || startIndex > val.Length - 1) {
-                throw new ArgumentOutOfRangeException(nameof(startIndex));
-            }
+		/// <summary>Slices a string from the start index to the end index.</summary>
+		/// <result>The sliced string.</result>
+		public static string Slice(this string val, int startIndex, int endIndex)
+		{
+			if (val.IsBlank())
+			{
+				throw new ArgumentNullException(nameof(val), "Value cannot be null or empty.");
+			}
 
-            // If the end index is negative, it will be counted from the end of the string.
-            endIndex = endIndex < 0 ? val.Length + endIndex : endIndex;
+			if (startIndex < 0 || startIndex > val.Length - 1)
+			{
+				throw new ArgumentOutOfRangeException(nameof(startIndex));
+			}
 
-            if (endIndex < 0 || endIndex < startIndex || endIndex > val.Length) {
-                throw new ArgumentOutOfRangeException(nameof(endIndex));
-            }
+			// If the end index is negative, it will be counted from the end of the string.
+			endIndex = endIndex < 0 ? val.Length + endIndex : endIndex;
 
-            return val.Substring(startIndex, endIndex - startIndex);
-        }
+			if (endIndex < 0 || endIndex < startIndex || endIndex > val.Length)
+			{
+				throw new ArgumentOutOfRangeException(nameof(endIndex));
+			}
 
-        /// <summary>
-        /// Converts the input string to an alphanumeric string, optionally allowing periods.
-        /// </summary>
-        /// <param name="input">The input string to be converted.</param>
-        /// <param name="allowPeriods">A boolean flag indicating whether periods should be allowed in the output string.</param>
-        /// <returns>
-        /// A new string containing only alphanumeric characters, underscores, and optionally periods.
-        /// If the input string is null or empty, an empty string is returned.
-        /// </returns>
-        public static string ConvertToAlphanumeric(this string input, bool allowPeriods = false) {
-            if (string.IsNullOrEmpty(input))
-                return string.Empty;
+			return val.Substring(startIndex, endIndex - startIndex);
+		}
 
-            List<char> filteredChars = new List<char>();
-            int lastValidIndex = -1;
+		/// <summary>
+		/// Converts the input string to an alphanumeric string, optionally allowing periods.
+		/// </summary>
+		/// <param name="input">The input string to be converted.</param>
+		/// <param name="allowPeriods">A boolean flag indicating whether periods should be allowed in the output string.</param>
+		/// <returns>
+		/// A new string containing only alphanumeric characters, underscores, and optionally periods.
+		/// If the input string is null or empty, an empty string is returned.
+		/// </returns>
+		public static string ConvertToAlphanumeric(this string input, bool allowPeriods = false)
+		{
+			if (string.IsNullOrEmpty(input))
+				return string.Empty;
 
-            // Iterate over the input string, filtering and determining valid start/end indices
-            foreach (char character in input
-                         .Where(character => char
-                             .IsLetterOrDigit(character) || character == '_' || (allowPeriods && character == '.'))
-                         .Where(character => filteredChars.Count != 0 || (!char.IsDigit(character) && character != '.'))) {
+			List<char> filteredChars = new List<char>();
+			int lastValidIndex = -1;
 
-                filteredChars.Add(character);
-                lastValidIndex = filteredChars.Count - 1; // Update lastValidIndex for valid characters
-            }
+			// Iterate over the input string, filtering and determining valid start/end indices
+			foreach (char character in input
+				         .Where(
+					         character => char
+						         .IsLetterOrDigit(character) || character == '_' || (allowPeriods && character == '.'))
+				         .Where(character => filteredChars.Count != 0 || (!char.IsDigit(character) && character != '.')))
+			{
+				filteredChars.Add(character);
+				lastValidIndex = filteredChars.Count - 1; // Update lastValidIndex for valid characters
+			}
 
-            // Remove trailing periods
-            while (lastValidIndex >= 0 && filteredChars[lastValidIndex] == '.') {
-                lastValidIndex--;
-            }
-    
-            // Return the filtered string
-            return lastValidIndex >= 0
-                ? new string(filteredChars.ToArray(), 0, lastValidIndex + 1) : string.Empty;
-        }
-        
-        // Rich text formatting, for Unity UI elements that support rich text.
-        public static string RichColor(this string text, string color) => $"<color={color}>{text}</color>";
-        public static string RichSize(this string text, int size) => $"<size={size}>{text}</size>";
-        public static string RichBold(this string text) => $"<b>{text}</b>";
-        public static string RichItalic(this string text) => $"<i>{text}</i>";
-        public static string RichUnderline(this string text) => $"<u>{text}</u>";
-        public static string RichStrikethrough(this string text) => $"<s>{text}</s>";
-        public static string RichFont(this string text, string font) => $"<font={font}>{text}</font>";
-        public static string RichAlign(this string text, string align) => $"<align={align}>{text}</align>";
-        public static string RichGradient(this string text, string color1, string color2) => $"<gradient={color1},{color2}>{text}</gradient>";
-        public static string RichRotation(this string text, float angle) => $"<rotate={angle}>{text}</rotate>";
-        public static string RichSpace(this string text, float space) => $"<space={space}>{text}</space>";
-    }
+			// Remove trailing periods
+			while (lastValidIndex >= 0 && filteredChars[lastValidIndex] == '.')
+			{
+				lastValidIndex--;
+			}
+
+			// Return the filtered string
+			return lastValidIndex >= 0
+				? new string(filteredChars.ToArray(), 0, lastValidIndex + 1)
+				: string.Empty;
+		}
+
+		// Rich text formatting, for Unity UI elements that support rich text.
+		public static string RichColor(this string text, string color) => $"<color={color}>{text}</color>";
+		public static string RichSize(this string text, int size) => $"<size={size}>{text}</size>";
+		public static string RichBold(this string text) => $"<b>{text}</b>";
+		public static string RichItalic(this string text) => $"<i>{text}</i>";
+		public static string RichUnderline(this string text) => $"<u>{text}</u>";
+		public static string RichStrikethrough(this string text) => $"<s>{text}</s>";
+		public static string RichFont(this string text, string font) => $"<font={font}>{text}</font>";
+		public static string RichAlign(this string text, string align) => $"<align={align}>{text}</align>";
+		public static string RichGradient(this string text, string color1, string color2) => $"<gradient={color1},{color2}>{text}</gradient>";
+		public static string RichRotation(this string text, float angle) => $"<rotate={angle}>{text}</rotate>";
+		public static string RichSpace(this string text, float space) => $"<space={space}>{text}</space>";
+
+
+		//Handle Text Case with safety
+		public static string ToAllCaps(this string text) => text?.ToUpper() ?? string.Empty;
+
+		public static string ToAllLower(this string text) => text?.ToLower() ?? string.Empty;
+
+		public static string ToProperCase(this string text)
+		{
+			if (string.IsNullOrEmpty(text)) return text ?? string.Empty;
+
+			string[] words = text.Split(' ');
+			for (int i = 0; i < words.Length; i++)
+			{
+				if (words[i].Length > 0)
+					words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1).ToLower();
+			}
+
+			return string.Join(" ", words);
+		}
+
+		public static string ToSpongeBobCase(this string text)
+		{
+			if (string.IsNullOrEmpty(text)) return text ?? string.Empty;
+
+			char[] chars = text.ToCharArray();
+			for (int i = 0; i < chars.Length; i++)
+			{
+				chars[i] = i % 2 == 0 ? char.ToLower(chars[i]) : char.ToUpper(chars[i]);
+			}
+
+			return new string(chars);
+		}
+	}
 }
