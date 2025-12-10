698aa06bc1fdc8bc18a2be6bf6d4c6fb942191bc
Travis Pierce <46234513+XuulMedia@users.noreply.github.com>
2025-12-09 18:15:33 -0700

    Updated Editor script assemblies

--- Utility Scripts/Editor/ProjectSetup.cs
+++ Utility Scripts/Editor/ProjectSetup.cs
@@ -1,194 +1,196 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using static System.IO.Path;
-using System.Threading.Tasks;
 using static UnityEditor.AssetDatabase;
 
-namespace ShoelaceStudios.Utilities
+namespace Utilities.Editor
 {
-    public static class ProjectSetup
-    {
-        [MenuItem("Tools/Setup/Import Essential Assets")]
-        static void ImportEssentialAssets()
-        {
-            Assets.ImportAsset("Odin Inspector and Serializer.unitypackage", "Sirenix/Editor ExtensionSystem");
-        }
+	public static class ProjectSetup
+	{
+		[MenuItem("Tools/Setup/Import Essential Assets")]
+		static void ImportEssentialAssets()
+		{
+			Assets.ImportAsset("Odin Inspector and Serializer.unitypackage", "Sirenix/Editor ExtensionSystem");
+		}
 
-        [MenuItem("Tools/Setup/Import Packages")]
-        static void InstallPackages()
-        {
-            string[] packages =
-            {
-                "git+https://github.com/adammyhre/Unity-Utils.git",
-                "git+https://github.com/adammyhre/Unity-Improved-Timers.git",
-                "https://github.com/Ryan-Pierce-Shoelace/Shoelace-SOAP-Variables.git#main",
-                "https://github.com/Ryan-Pierce-Shoelace/Shoelace-SOAP-Events.git#main",
-                "https://github.com/Ryan-Pierce-Shoelace/Shoelace-Audio-System.git#main",
-                "https://github.com/Ryan-Pierce-Shoelace/Shoelace-Grid-System.git#main",
-                "https://github.com/Ryan-Pierce-Shoelace/Shoelace-Region-System.git#main"
-            };
+		[MenuItem("Tools/Setup/Import Packages")]
+		static void InstallPackages()
+		{
+			string[] packages =
+			{
+				"git+https://github.com/adammyhre/Unity-Utils.git",
+				"git+https://github.com/adammyhre/Unity-Improved-Timers.git",
+				"https://github.com/Ryan-Pierce-Shoelace/Shoelace-SOAP-Variables.git#main",
+				"https://github.com/Ryan-Pierce-Shoelace/Shoelace-SOAP-Events.git#main",
+				"https://github.com/Ryan-Pierce-Shoelace/Shoelace-Audio-System.git#main",
+				"https://github.com/Ryan-Pierce-Shoelace/Shoelace-Grid-System.git#main",
+				"https://github.com/Ryan-Pierce-Shoelace/Shoelace-Region-System.git#main"
+			};
 
-            Packages.InstallPackages(packages);
-        }
+			Packages.InstallPackages(packages);
+		}
 
-        [MenuItem("Tools/Setup/Create Folder Structure")]
-        static void CreateFolderStructure()
-        {
-            Folders.Create("_Project",
-                "_Scripts",
-                "Prefabs", "Prefabs/UI", "Prefabs/Gameplay",
-                "Art",
-                "Art/Sprites", "Art/Sprites/Tilemaps", "Art/Sprites/Objects", "Art/Sprites/Characters",
-                "Art/Sprites/Characters/Example", "Art/Sprites/Characters/Example/Spritesheet",
-                "Art/Sprites/Characters/Example/Animations", "Art/Sprites/Characters/Example/Animator",
-                "Fonts",
-                "Models",
-                "Models/Objects", "Models/Objects/Clutter", "Models/Objects/Item", "Models/Objects/Structure",
-                "Models/Objects/Item/Example", "Models/Objects/Item/Example/Textures",
-                "Models/Objects/Item/Example/Shader", "Models/Objects/Item/Example/Material",
-                "Models/Character", "Models/Character/Example",
-                "Models/Character/Example/Model",
-                "Models/Character/Example/Animation", "Models/Character/Example/Animation/Idle",
-                "Models/Character/Example/Animation/Run", "Models/Character/Example/Animation/Attack",
-                "Models/Character/Example/Materials", "Models/Character/Example/Materials/Textures",
-                "Models/Character/Example/Materials/Shader", "Models/Character/Example/Materials/MaterialAssets",
-                "UI",
-                "UI/Images",
-                "UI/Images/Panels",
-                "UI/Images/Icons"
-            );
-            Refresh();
-            Folders.Move("_Project", "Scenes");
-            Folders.Move("_Project", "Settings");
-            Folders.Create("_Project",
-                "Scenes/Development",
-                "Scenes/Levels",
-                "Scenes/SubScenes",
-                "Scenes/Manager");
+		[MenuItem("Tools/Setup/Create Folder Structure")]
+		static void CreateFolderStructure()
+		{
+			Folders.Create(
+				"_Project",
+				"_Scripts",
+				"Prefabs", "Prefabs/UI", "Prefabs/Gameplay",
+				"Art",
+				"Art/Sprites", "Art/Sprites/Tilemaps", "Art/Sprites/Objects", "Art/Sprites/Characters",
+				"Art/Sprites/Characters/Example", "Art/Sprites/Characters/Example/Spritesheet",
+				"Art/Sprites/Characters/Example/Animations", "Art/Sprites/Characters/Example/Animator",
+				"Fonts",
+				"Models",
+				"Models/Objects", "Models/Objects/Clutter", "Models/Objects/Item", "Models/Objects/Structure",
+				"Models/Objects/Item/Example", "Models/Objects/Item/Example/Textures",
+				"Models/Objects/Item/Example/Shader", "Models/Objects/Item/Example/Material",
+				"Models/Character", "Models/Character/Example",
+				"Models/Character/Example/Model",
+				"Models/Character/Example/Animation", "Models/Character/Example/Animation/Idle",
+				"Models/Character/Example/Animation/Run", "Models/Character/Example/Animation/Attack",
+				"Models/Character/Example/Materials", "Models/Character/Example/Materials/Textures",
+				"Models/Character/Example/Materials/Shader", "Models/Character/Example/Materials/MaterialAssets",
+				"UI",
+				"UI/Images",
+				"UI/Images/Panels",
+				"UI/Images/Icons"
+			);
+			Refresh();
+			Folders.Move("_Project", "Scenes");
+			Folders.Move("_Project", "Settings");
+			Folders.Create(
+				"_Project",
+				"Scenes/Development",
+				"Scenes/Levels",
+				"Scenes/SubScenes",
+				"Scenes/Manager");
 
-            Folders.Delete("TutorialInfo");
-            Refresh();
-            const string pathToInputActions = "Assets/InputSystem_Actions.inputactions";
-            string destination = "Assets/_Project/Settings/InputSystem_Actions.inputactions";
-            MoveAsset(pathToInputActions, destination);
-            const string pathToReadme = "Assets/Readme.asset";
-            DeleteAsset(pathToReadme);
-            Refresh();
-        }
+			Folders.Delete("TutorialInfo");
+			Refresh();
+			const string pathToInputActions = "Assets/InputSystem_Actions.inputactions";
+			string destination = "Assets/_Project/Settings/InputSystem_Actions.inputactions";
+			MoveAsset(pathToInputActions, destination);
+			const string pathToReadme = "Assets/Readme.asset";
+			DeleteAsset(pathToReadme);
+			Refresh();
+		}
 
-        static class Assets
-        {
-            public static void ImportAsset(string asset, string folder)
-            {
-                string basePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
-                string assetsFolder = Combine(basePath, "Unity/Asset Store-5.x");
+		static class Assets
+		{
+			public static void ImportAsset(string asset, string folder)
+			{
+				string basePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+				string assetsFolder = Combine(basePath, "Unity/Asset Store-5.x");
 
-                ImportPackage(Combine(assetsFolder, folder, asset), false);
-            }
-        }
+				ImportPackage(Combine(assetsFolder, folder, asset), false);
+			}
+		}
 
-        static class Packages
-        {
-            static AddRequest request;
-            private static Queue<string> packagesToInstall = new();
+		static class Packages
+		{
+			static AddRequest request;
+			private static Queue<string> packagesToInstall = new();
 
-            static async void StartNextPackageInstallation()
-            {
-                request = Client.Add(packagesToInstall.Dequeue());
+			static async void StartNextPackageInstallation()
+			{
+				request = Client.Add(packagesToInstall.Dequeue());
 
-                while (!request.IsCompleted)
-                {
-                    Debug.Log("Downloading : " + packagesToInstall.Count + " packages remaining");
-                    await Task.Delay(500);
-                }
+				while (!request.IsCompleted)
+				{
+					Debug.Log("Downloading : " + packagesToInstall.Count + " packages remaining");
+					await Task.Delay(500);
+				}
 
-                if (request.Status == StatusCode.Success) Debug.Log("Installed : " + request.Result.packageId);
-                else if (request.Status == StatusCode.Failure)
-                    Debug.LogError("Failed to install : " + request.Result.packageId + " ||| " + request.Error.message);
+				if (request.Status == StatusCode.Success) Debug.Log("Installed : " + request.Result.packageId);
+				else if (request.Status == StatusCode.Failure)
+					Debug.LogError("Failed to install : " + request.Result.packageId + " ||| " + request.Error.message);
 
-                if (packagesToInstall.Count != 0)
-                {
-                    await Task.Delay(1000);
-                    StartNextPackageInstallation();
-                }
-            }
+				if (packagesToInstall.Count != 0)
+				{
+					await Task.Delay(1000);
+					StartNextPackageInstallation();
+				}
+			}
 
-            public static void InstallPackages(string[] packages)
-            {
-                foreach (string package in packages)
-                {
-                    packagesToInstall.Enqueue(package);
-                }
+			public static void InstallPackages(string[] packages)
+			{
+				foreach (string package in packages)
+				{
+					packagesToInstall.Enqueue(package);
+				}
 
-                if (packagesToInstall.Count > 0)
-                {
-                    StartNextPackageInstallation();
-                }
-            }
+				if (packagesToInstall.Count > 0)
+				{
+					StartNextPackageInstallation();
+				}
+			}
+		}
 
+		static class Folders
+		{
+			public static void Delete(string folderName)
+			{
+				string pathToDelete = $"Assets/{folderName}";
 
-        }
+				if (IsValidFolder(pathToDelete))
+				{
+					DeleteAsset(pathToDelete);
+				}
+			}
 
-        static class Folders
-        {
-            public static void Delete(string folderName)
-            {
-                string pathToDelete = $"Assets/{folderName}";
+			public static void Move(string newParent, string folderName)
+			{
+				string sourcePath = $"Assets/{folderName}";
+				if (IsValidFolder(sourcePath))
+				{
+					string destinationPath = $"Assets/{newParent}/{folderName}";
+					string error = MoveAsset(sourcePath, destinationPath);
 
-                if (IsValidFolder(pathToDelete))
-                {
-                    DeleteAsset(pathToDelete);
-                }
-            }
+					if (!string.IsNullOrEmpty(error))
+					{
+						Debug.LogError(error);
+					}
+				}
+			}
 
-            public static void Move(string newParent, string folderName)
-            {
-                string sourcePath = $"Assets/{folderName}";
-                if (IsValidFolder(sourcePath))
-                {
-                    string destinationPath = $"Assets/{newParent}/{folderName}";
-                    string error = MoveAsset(sourcePath, destinationPath);
+			static void CreateSubFolders(string rootPath, string folderHierarchy)
+			{
+				var folders = folderHierarchy.Split('/');
+				var currentPath = rootPath;
 
-                    if (!string.IsNullOrEmpty(error))
-                    {
-                        Debug.LogError(error);
-                    }
-                }
-            }
+				foreach (var folder in folders)
+				{
+					currentPath = Combine(currentPath, folder);
 
-            static void CreateSubFolders(string rootPath, string folderHierarchy)
-            {
-                var folders = folderHierarchy.Split('/');
-                var currentPath = rootPath;
+					if (!Directory.Exists(currentPath))
+					{
+						Directory.CreateDirectory(currentPath);
+					}
+				}
+			}
 
-                foreach (var folder in folders)
-                {
-                    currentPath = Combine(currentPath, folder);
+			public static void Create(string root, params string[] folders)
+			{
+				var fullpath = Path.Combine(Application.dataPath, root);
+				if (!Directory.Exists(fullpath))
+				{
+					Directory.CreateDirectory(fullpath);
+				}
 
-                    if (!Directory.Exists(currentPath))
-                    {
-                        Directory.CreateDirectory(currentPath);
-                    }
-                }
-            }
-
-            public static void Create(string root, params string[] folders)
-            {
-                var fullpath = Path.Combine(Application.dataPath, root);
-                if (!Directory.Exists(fullpath))
-                {
-                    Directory.CreateDirectory(fullpath);
-                }
-
-                foreach (var folder in folders)
-                {
-                    CreateSubFolders(fullpath, folder);
-                }
-            }
-        }
-    }
+				foreach (var folder in folders)
+				{
+					CreateSubFolders(fullpath, folder);
+				}
+			}
+		}
+	}
 }
+#endif
