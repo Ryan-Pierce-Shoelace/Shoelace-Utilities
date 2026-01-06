c37fc7262127da580dce7fa31a8b5792ac4ef6de
Ryan Pierce <shoelaces347@gmail.com>
2026-01-06 15:15:24 -0500

    Added new Custom Timer

--- Utility Scripts/ImprovedTimers/Timers/CountdownTimer.cs
+++ Utility Scripts/ImprovedTimers/Timers/CountdownTimer.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 
-namespace ImprovedTimers {
+namespace ImprovedTimers
+{
     /// <summary>
     /// Timer that counts down from a specific value to zero.
     /// </summary>
-    public class CountdownTimer : Timer {
-        public CountdownTimer(float value) : base(value) { }
+    public class CountdownTimer : Timer
+    {
+        public CountdownTimer(float value) : base(value)
+        {
+        }
 
-        public override void Tick() {
-            if (IsRunning && CurrentTime > 0) {
+        public override void Tick()
+        {
+            if (IsRunning && CurrentTime > 0)
+            {
                 CurrentTime -= Time.deltaTime;
             }
 
-            if (IsRunning && CurrentTime <= 0) {
+            if (IsRunning && CurrentTime <= 0)
+            {
                 Stop();
             }
         }
